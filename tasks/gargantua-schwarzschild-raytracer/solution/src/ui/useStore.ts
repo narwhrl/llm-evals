0054abc1579fe4e79eba407fb80a18521b83c087
narwhrl <narwhrl@gmail.com>
2026-09-23 02:08:04 +0800

    chore: clear previous glm-5.3 candidate for independent rewrite

--- tasks/gargantua-schwarzschild-raytracer/solution/src/ui/useStore.ts
+++
@@ -1,6 +0,0 @@
-import { useSyncExternalStore } from 'react';
-import { store, type GargantuaState } from '../state/store';
-
-export function useStoreState(): GargantuaState {
-  return useSyncExternalStore(store.subscribe, store.getState);
-}
